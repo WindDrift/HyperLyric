1a82c8269d077c8cf5bad56fe79a0446008a9db8
lidesheng <169997481+limczhh@users.noreply.github.com>
2026-08-18 16:47:46 +0800

    1937-7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.lidesheng.hyperlyric"
         minSdk = 33
         targetSdk = 37
-        versionCode = 1936
-        versionName = "7.0"
+        versionCode = 1937
+        versionName = "7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
